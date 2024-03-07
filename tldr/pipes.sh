18990cb95ef21ba8cd2fc5145d3a9f2c0946850b
ivuorinen <ivuorinen@users.noreply.github.com>
2024-03-07 00:10:46 +0000

    Update cheatsheets

---
+++ tldr/pipes.sh
@@ -0,0 +1,29 @@
+---
+syntax: markdown
+tags: [tldr, common]
+source: https://github.com/tldr-pages/tldr.git
+---
+# pipes.sh
+
+> A bash script which draws randomly pathed pipes over the terminal.
+> More information: <https://github.com/pipeseroni/pipes.sh>.
+
+- Change the pa[t]tern of the pipes:
+
+`pipes.sh -t {{0..9}}`
+
+- Change the [c]olor of the pipes:
+
+`pipes.sh -c {{0..7}}`
+
+- Change the [f]ramerate of the pipes:
+
+`pipes.sh -f {{20..100}}`
+
+- Disable [C]olors:
+
+`pipes.sh -C`
+
+- Display [v]ersion:
+
+`pipes.sh -v`
