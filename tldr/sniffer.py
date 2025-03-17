9cdd033c956e52d218e12ca145db8e81d33c66e1
ivuorinen <ivuorinen@users.noreply.github.com>
2025-03-17 00:19:24 +0000

    Update cheatsheets

---
+++ tldr/sniffer.py
@@ -0,0 +1,22 @@
+---
+syntax: markdown
+tags: [tldr, common]
+source: https://github.com/tldr-pages/tldr.git
+---
+# sniffer.py
+
+> Capture and display network packets for specified protocols using raw sockets.
+> Part of the Impacket suite.
+> More information: <https://github.com/fortra/impacket>.
+
+- Capture packets for default protocols (ICMP, TCP, UDP):
+
+`sniffer.py`
+
+- Capture packets for specific protocols (e.g., ICMP, TCP):
+
+`sniffer.py {{protocol1}} {{protocol2}}`
+
+- Capture packets for specific protocols (e.g., TCP):
+
+`sniffer.py tcp`
