ffe393201e024bf0f1b2ea85a033703df1940055
ivuorinen <ivuorinen@users.noreply.github.com>
2026-01-09 00:22:15 +0000

    Update cheatsheets

--- tldr/windows/vol.py
+++ tldr/windows/vol.py
@@ -11,19 +11,19 @@ source: https://github.com/tldr-pages/tldr.git
 
 - Get information about a memory dump file:
 
-`python3 vol.py {{[-f|--filename]}} {{path/to/memory_dump_file}} windows.info`
+`python3 vol.py {{[-f|--filename]}} {{path	o\memory_dump_file}} windows.info`
 
 - List active processes:
 
-`python3 vol.py {{[-f|--filename]}} {{path/to/memory_dump_file}} windows.pslist`
+`python3 vol.py {{[-f|--filename]}} {{path	o\memory_dump_file}} windows.pslist`
 
 - List hashes of users on system:
 
-`python3 vol.py {{[-f|--filename]}} {{path/to/memory_dump_file}} windows.hashdump`
+`python3 vol.py {{[-f|--filename]}} {{path	o\memory_dump_file}} windows.hashdump`
 
 - List active network connections:
 
-`python3 vol.py {{[-f|--filename]}} {{path/to/memory_dump_file}} windows.netstat`
+`python3 vol.py {{[-f|--filename]}} {{path	o\memory_dump_file}} windows.netstat`
 
 - Display help:
 
